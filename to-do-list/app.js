5763fce859d88eef3ef97dbc5d8c7fd79127a7b8
Luiz F Takano <luizfltakano@gmail.com>
2023-02-02 00:25:17 +0100

    added comments to server file

--- to-do-list/app.js
+++ to-do-list/app.js
@@ -1,18 +1,20 @@
 const express = require("express");
-
 const app = express();
 
+// Item arrays
 const items = [];
+const workItems = [];
 
+// Set view engine and middleware
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Route for main list page
 app.get("/", (req, res) => {
+	// Get current date and format it
 	const today = new Date();
-	const currentDate = today.getDate();
-
 	const options = {
 		year: "numeric",
 		month: "long",
@@ -20,13 +22,41 @@ app.get("/", (req, res) => {
 	};
 	const formattedDate = today.toLocaleDateString("en-UK", options);
 
-	res.render("list", { date: formattedDate, itemsList: items });
+	// Render list page with current date and items list
+	res.render("list", { list: formattedDate, itemsList: items });
 });
 
-app.post("/new-item", (req, res) => {
+// Route for work list page
+app.get("/work", (req, res) => {
+	// Render list page with "Work List" title and work items list
+	res.render("list", { list: "Work List", itemsList: workItems });
+});
+
+// Route to add items to main list or work list
+app.post("/", (req, res) => {
+	// Get item and form type from request body
 	const item = req.body.newItem;
-	items.push(item);
-	res.redirect("/");
+	const form = req.body.button;
+
+	// Add item to work list or main list
+	if (form === "Work") {
+		workItems.push(item);
+		res.redirect("/work");
+	} else {
+		items.push(item);
+		res.redirect("/");
+	}
 });
 
+// Route to add items to work list
+app.post("/work", (req, res) => {
+	// Get item from request body
+	const item = req.body.newItem;
+
+	// Add item to work list
+	workItems.push(item);
+	res.redirect("/work");
+});
+
+// Start server on port 3000
 app.listen(3000, () => console.log("Server started on port 3000"));
