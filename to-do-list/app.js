be68ead91248496440001783834eb23587bb53b1
Luiz F Takano <luizfltakano@gmail.com>
2023-02-01 13:51:23 +0100

    first commit w todo list

---
+++ to-do-list/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const app = express();
+
+const items = [];
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+	const today = new Date();
+	const currentDate = today.getDate();
+
+	const options = {
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+	};
+	const formattedDate = today.toLocaleDateString("en-UK", options);
+
+	res.render("list", { date: formattedDate, itemsList: items });
+});
+
+app.post("/new-item", (req, res) => {
+	const item = req.body.newItem;
+	items.push(item);
+	res.redirect("/");
+});
+
+app.listen(3000, () => console.log("Server started on port 3000"));
