847b026387ce92a4ea8d3617897fc5bace51837b
Luiz F Takano <luizfltakano@gmail.com>
2023-02-01 18:43:01 +0100

    todo list server and front finished

--- to-do-list/app.js
+++ to-do-list/app.js
@@ -7,6 +7,7 @@ const items = [];
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
 	const today = new Date();
