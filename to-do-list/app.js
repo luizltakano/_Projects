ad729c536974a43e2ee6cc90650a0c2187d3b005
Luiz F Takano <luizfltakano@gmail.com>
2023-02-04 14:18:52 +0100

    add comments

--- to-do-list/app.js
+++ to-do-list/app.js
@@ -10,18 +10,27 @@ const {
 const app = express();
 
 // Set view engine and middleware
+// Specify view engine as EJS
 app.set("view engine", "ejs");
+
+// Parse incoming request bodies in a middleware before handlers
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from the public directory
 app.use(express.static("public"));
 
 // Route for main list page
+// Renders the main list page
 app.get("/", (req, res) => {
+	// Call the getTasks function from manage-db module to get list tasks
 	getTasks("home", res);
 });
 
 // Route for work list page
+// Renders the work list page
 app.get("/work", (req, res) => {
+	// Call the getTasks function from manage-db module to get work tasks
 	getTasks("work", res);
 });
 
@@ -38,25 +47,21 @@ app.post("/", (req, res) => {
 		id: uuid(),
 	};
 
+	// Call the addTaskToList function from manage-db module to add task to the specified list
 	addTaskToList(list, task, res);
 });
 
 // Route to delete items from main list or work list
 app.post("/delete", (req, res) => {
+	// Get the list name from the base URL of the referer header
 	const baseUrl = req.headers.referer.substring(
 		req.headers.referer.lastIndexOf("/") + 1
 	);
 
+	// Get the task id from the request body
 	const taskId = req.body.checkbox;
+	// Call the markTaskCompleted function from manage-db module to mark the specified task as completed
 	markTaskCompleted(!baseUrl ? "home" : baseUrl, taskId, res);
-
-	// Task.findByIdAndRemove(taskId, (err) => {
-	// 	if (err) {
-	// 		console.log(err);
-	// 	} else {
-	// res.redirect("/" + baseUrl);
-	// 	}
-	// });
 });
 
 // Start server on port 3000
--- to-do-list/modules/manage-db.js
+++ to-do-list/modules/manage-db.js
@@ -1,12 +1,11 @@
+// Connect to MongoDB with Mongoose
 const mongoose = require("mongoose");
-
 mongoose.connect(process.env.PROD_PORT, {
 	useNewUrlParser: true,
 });
-
 mongoose.set("strictQuery", true);
 
-//List schema to be udpated with tasks
+// Define List schema
 const listSchema = mongoose.Schema({
 	name: {
 		type: String,
@@ -19,10 +18,9 @@ const listSchema = mongoose.Schema({
 		unique: true,
 	},
 });
-
 const List = mongoose.model("List", listSchema);
 
-//Task schema to create and delete tasks
+// Define Task schema
 const taskSchema = mongoose.Schema({
 	title: {
 		type: String,
@@ -36,69 +34,47 @@ const taskSchema = mongoose.Schema({
 		unique: true,
 	},
 });
-
 const Task = mongoose.model("Task", taskSchema);
 
-//Get tasks available depending on route
+// Get tasks based on route
 exports.getTasks = (route, res) => {
-	List.find(
-		{
-			name: route,
-		},
-		(err, list) => {
-			if (err) {
-				console.log(err);
-			} else {
-				const taskList = !list[0].tasks ? [] : list[0].tasks;
-				// Render list page with current date and items list
-				res.render("list", {
-					list: route.charAt(0).toUpperCase() + route.substr(1).toLowerCase(),
-					itemsList: taskList,
-				});
-			}
+	List.find({ name: route }, (err, list) => {
+		if (err) {
+			console.log(err);
+		} else {
+			const taskList = !list[0].tasks ? [] : list[0].tasks;
+			res.render("list", {
+				list: route.charAt(0).toUpperCase() + route.substr(1).toLowerCase(),
+				itemsList: taskList,
+			});
 		}
-	);
+	});
 };
 
-//Add tasks to a list
+// Add task to list
 exports.addTaskToList = (list, task, res) => {
 	const newTask = new Task(task);
-	List.updateOne(
-		{ name: list },
-		{
-			$push: {
-				tasks: newTask,
-			},
-		},
-		(err) => {
-			if (err) {
-				console.log(err);
-			} else {
-				res.redirect("/" + (list === "home" ? "" : list));
-			}
+	List.updateOne({ name: list }, { $push: { tasks: newTask } }, (err) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.redirect("/" + (list === "home" ? "" : list));
 		}
-	);
+	});
 };
 
-//Mark task as completed
+// Mark task as completed
 exports.markTaskCompleted = (list, id, res) => {
 	const closedAt = new Date();
-
 	List.updateOne(
 		{
 			name: list,
 			tasks: {
-				$elemMatch: {
-					id: id,
-				},
+				$elemMatch: { id: id },
 			},
 		},
-		{
-			$set: { "tasks.$.closedAt": closedAt },
-		},
-		{
-			arrayFilters: [{ "tasks._id": id }],
-		},
+		{ $set: { "tasks.$.closedAt": closedAt } },
+		{ arrayFilters: [{ "tasks._id": id }] },
 		(err) => {
 			if (err) {
 				console.log(err);
